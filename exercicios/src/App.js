39246da7d3511f2f4a53410b1fa288380018e085
Henrique Melanda <henrique.melanda@vagas.com.br>
2019-10-29 14:35:43 -0300

    Conditional Rendering

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Simples from './components/Simples';
+import ParImpar from './components/ParImpar';
 
 export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
         <Simples texto='Hello React Native!!! o/' />
+        <ParImpar numero={32} />
       </View>
     )
   }
