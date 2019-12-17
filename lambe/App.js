64991783dcc6a82d69df588fbeadd64c4f64b964
Henrique Melanda <henrique.melanda@vagas.com.br>
2019-12-17 20:37:16 -0300

    Component header

---
+++ lambe/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Header from './src/componentes/Header'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
